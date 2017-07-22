960d5927326b88959f467c55fc7ae85a7917724f
enriqueav <arriaga.v.enrique@gmail.com>
2017-07-22 12:47:33 +0200

    Create the-great-xor.java
    https://www.hackerrank.com/challenges/the-great-xor/problem

---
+++ bit-manipulation/the-great-xor.java
@@ -0,0 +1,28 @@
+//  how this works
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static long theGreatXor(long x){
+      long a=1;
+      while(a<=x){
+          a<<=1; 
+      }
+      return a-x-1;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int q = in.nextInt();
+        for(int a0 = 0; a0 < q; a0++){
+            long x = in.nextLong();
+            long result = theGreatXor(x);
+            System.out.println(result);
+        }
+    }
+}
