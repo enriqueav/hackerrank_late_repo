94e7b73b7cebfa53140a2cd5f9d347c809c0942b
enriqueav <arriaga.v.enrique@gmail.com>
2017-07-23 11:34:26 +0200

    Create binary-search-tree-insertion.java
    https://www.hackerrank.com/challenges/binary-search-tree-insertion/problem

---
+++ trees/binary-search-tree-insertion.java
@@ -0,0 +1,24 @@
+ /* Node is defined as :
+ class Node 
+    int data;
+    Node left;
+    Node right;
+    
+    */
+
+static Node Insert(Node root,int value)
+{
+    if( root == null ){
+        Node n = new Node();
+        n.data = value;
+        return n;        
+    }
+    if( value > root.data ){
+        root.right = Insert(root.right,value);
+    } else if( value < root.data ){
+        root.left = Insert(root.left,value);
+    }
+    return root;
+}
+
+
